95c9882145242dbbae778ba62d6fefef48e92388
juan Manuel Sanchez Mesa <juanma_sme@hotmail.com>
2025-03-08 20:44:22 +0100

    clase Universidad completada

--- Poo/App_universidad/src/universitat/Universitat.java
+++ Poo/App_universidad/src/universitat/Universitat.java
@@ -6,133 +6,162 @@
 package universitat;
 
 
+import java.util.Scanner;
+
 /**
- *
  * @author fgarin
  */
 public class Universitat {
+    private final static Scanner DADES = new Scanner(System.in);
 
     private String nomUniversitat;
     private String ubicacioSeu;
     private Campus[] campus = new Campus[5];
     private int pCampus = 0; //Primera posició buida de l'array de campus
 
-    /*
-     * TODO CONSTRUCTOR
-     *
-     * Nom del mètode: Universitat
-     *
-     * Paràmetres: valors per tots els atributs de la classe menys els arrays.
-     *
-     * Accions:
-     * - Assignar als atributs corresponents els valors passats com a paràmetres.
-     */
-    
-    
-    
 
-    /**
-     * TODO Heu d'implementar tots els mètodes accessors possibles.
-     */
-    
-   
+    public Universitat(String nomUniversitat, String ubicacioSeu) {
+        this.nomUniversitat = nomUniversitat;
+        this.ubicacioSeu = ubicacioSeu;
+    }
 
-    /**
-     * TODO
-     *
-     * Nom del mètode: addUniversitat
-     *
-     * Paràmetres: cap
-     * 
-     * Accions:
-     * - Demanar a l'usuari les dades per consola per crear una nova universitat.
-     *   Les dades a demanar són les que necessita el constructor.
-     * 
-     * Retorn: Objecte Universitat creat.
-     */
-    
+    public String getNomUniversitat() {
+        return nomUniversitat;
+    }
 
-    /**
-     * TODO
-     *
-     * Nom del mètode: updateUniversitat
-     * 
-     * Paràmetres: cap
-     * 
-     * Accions:
-     * - Demanar a l'usuari que introdueixi les noves dades de la universitat i
-     *   modificar els atributs corresponents d'aquesta universitat. 
-     *   Els únics atributs que modificarem
-     *   són els que hem inicialitzat en el constructor amb els paràmetres passats.
-     * - Li heu de mostrar a l'usuari els valors dels atributs abans de modificar-los.
-     * 
-     * Retorn: cap
-     */
-    
-    
+    public void setNomUniversitat(String nomUniversitat) {
+        this.nomUniversitat = nomUniversitat;
+    }
 
-    /**
-     * TODO
-     *
-     * Nom del mètode: costManteniment
-     *  
-     * Paràmetres: cap
-     * 
-     * Accions:
-     * - Calcular cost de manteniment dels campus de la universitat a partir dels costos de manteniment
-     *   de tots els campus de la universitat.
-     * 
-     * Retorn: Cost de manteniment total de la universitat (double).
-     */
-    
+    public String getUbicacioSeu() {
+        return ubicacioSeu;
+    }
 
-    /**
-     * TODO
-     *
-     * Nom del mètode: showUniversitat
-     * 
-     * Paràmetres: cap
-     * 
-     * Accions:
-     * - Mètode per imprimir les dades de la universitat actual i el cost del seu manteniment.
-     * 
-     * Retorn: cap
-     */
- 
+    public void setUbicacioSeu(String ubicacioSeu) {
+        this.ubicacioSeu = ubicacioSeu;
+    }
+
+    public Campus[] getCampus() {
+        return campus;
+    }
+
+    public void setCampus(Campus[] campus) {
+        this.campus = campus;
+    }
+
+    public int getpCampus() {
+        return pCampus;
+    }
+
+    public void setpCampus(int pCampus) {
+        this.pCampus = pCampus;
+    }
+
+
+    public Universitat addUniversitat() {
+        System.out.println("Introdueix el nom de la Universitat:");
+        String nomUniversitat = DADES.nextLine();
+
+        System.out.println("Introdueix la ubicació:");
+        String ubicacio = DADES.nextLine();
+
+
+        return new Universitat(nomUniversitat, ubicacio);
+    }
+
+    public void updateUniversitat() {
+        System.out.println("Dades actuals de la universitat:");
+        System.out.println("Nom Universitat: " + getNomUniversitat());
+        System.out.println("Ubicació de la seu: " + getUbicacioSeu());
+
+        System.out.println("\nIntrodueix noves dades de la universitat ");
+
+        System.out.println("Nou nom de la universitat: ");
+        setNomUniversitat(DADES.nextLine());
+
+        System.out.println("Nova ubicació: ");
+        setUbicacioSeu(DADES.nextLine());
+
+        System.out.println("\nDades actualizades correctament");
+    }
+
+
+    public double costManteniment() {
+        double costTotal = 0;
+
+        for (int i = 0; i < getpCampus(); i++) {
+            costTotal += getCampus()[i].costManteniment();
+        }
+        return costTotal;
+    }
+
+    public void showUniversitat() {
+
+        System.out.println("\n===== Informació de la Universitat =====");
+        System.out.println("Nom: " + getNomUniversitat());
+        System.out.println("Ubicació: " + getUbicacioSeu());
+
+
+        double costTotalManteniment = costManteniment();
+        System.out.println("Cost total de manteniment: " + costTotalManteniment + " €");
+        System.out.println("====================================");
+    }
 
 
     /**
      * Campus
-     * 
+     * <p>
      * TODO
-     *
+     * <p>
      * Nom del mètode: addCampus
-     *  
+     * <p>
      * Paràmetres: cap
-     * 
+     * <p>
      * Accions:
      * - Afegeix un nou campus a l'array de campus de la universitat
-     *   si aquest no existeix. 
-     *   Per afegir-lo heu de fer servir el mètode de la classe Campus escaient
-     *   i per comprovar la seva existència el mètode d'aquesta classe que ens
-     *   ajuda en aquesta tasca.
-     * - Actualitza la posició de l'array de campus si s'afegeix el campus. 
+     * si aquest no existeix.
+     * Per afegir-lo heu de fer servir el mètode de la classe Campus escaient
+     * i per comprovar la seva existència el mètode d'aquesta classe que ens
+     * ajuda en aquesta tasca.
+     * - Actualitza la posició de l'array de campus si s'afegeix el campus.
      * - Mostra el missatge "El campus ja existeix" si no s'ha afegit.
-     * 
+     * <p>
      * Retorn: cap
      */
-    
+    public void addCampus() {
+        Campus nouCampus = new Campus("", "");
+        boolean campusExisteix = false;
+        boolean espai = true;
+
+        for(int i = 0; i < getpCampus(); i++){
+
+            if(getCampus()[i].getNomCampus().equalsIgnoreCase(nouCampus.getNomCampus())) {
+                System.out.println("El campus ja existeix");
+                campusExisteix = true;
+            }
+        }
+        if(getpCampus() > 4){
+            espai = false;
+            System.out.println("No hi ha més espai per afegir campus");
+        }
+
+        if(!campusExisteix && espai){
+            getCampus()[getpCampus()] = nouCampus;
+            setpCampus(getpCampus()+1);
+            System.out.println("Campus afegit correctament");
+        }
+    }
+
 
     /**
-     *
      * Nom del mètode: selectCampus
-     *
+     * <p>
      * Paràmetres: cap
-     * 
+     * <p>
      * Accions:
      * - Seleciona un campus de l'array de campus de la universitat
-     *   a partir del seu nom.
-     * 
+     * a partir del seu nom.
+     * <p>
      * Retorn: La posició del campus seleccionat o -1 si no existeix.
      */
     public int selectCampus(String nom) {
