ffd0c02c08ee9ee601df39f2b9e641c4382c5ca1
juan Manuel Sanchez Mesa <juanma_sme@hotmail.com>
2025-03-09 10:27:13 +0100

    menu campus completo

--- Poo/App_universidad/src/principal/Application.java
+++ Poo/App_universidad/src/principal/Application.java
@@ -1,16 +1,19 @@
 package principal;
 
+import universitat.Campus;
 import universitat.Universitat;
+
+import java.util.Scanner;
 
 
 /**
- *
  * @author fgarin
  */
 public class Application {
+    private final static Scanner DADES = new Scanner(System.in);
 
     private static Universitat[] universitats = new Universitat[10];
-    private static int Universitat = 0; //Primera posició buida del vector universitats
+    private static int pUniversitat = 0; //Primera posició buida del vector universitats
     private static Universitat universitatActual = null;
 
     public static void main(String[] args) {
@@ -28,7 +31,7 @@ public class Application {
             System.out.println("\n3. Gestió d'aules estàndard");
             System.out.println("\n4. Gestió d'aules d'informàtica");
             System.out.println("\n5. Gestió de laboratoris");
-            System.out.println("\n");       
+            System.out.println("\n");
 
             opcio = Integer.parseInt(DADES.nextLine());
 
@@ -84,7 +87,7 @@ public class Application {
             System.out.println("\n2. Seleccionar");
             System.out.println("\n3. Modificar");
             System.out.println("\n4. Llistar");
-            System.out.println("\n");  
+            System.out.println("\n");
 
             opcio = Integer.parseInt(DADES.nextLine());
 
@@ -136,18 +139,18 @@ public class Application {
 
     /*
      * TODO
-     * 
+     *
      * Nom del mètode: menuCampus
-     *     
+     *
      * Heu de desenvolupar el menu Campus amb les opcions que podeu veure.
      * Nota: penseu que quan arribem aquí, l'atribut universitatActual no és null.
-     * 
+     *
      * Opció 0. Sortir          --> Surt del menú i retorna al menú principal.
-     * Opció 1. Alta            --> Crea una Campus del campus actual. Noteu que Universitat sap crear Campus.        
+     * Opció 1. Alta            --> Crea una Campus del campus actual. Noteu que Universitat sap crear Campus.
      * Opció 2. Modificar       --> Permet modificar una Campus del campus actual.
      * (per comprovar l'existència d'una campus tenim un mètode en la classe Universitat que ens ajuda).
      * Opció 3. Llista Campus   --> Imprimeix les dades dels campus del campus actual.
-     * 
+     *
      * A més, heu de fer una estructura iterativa per tornar a mostrar el menú sempre que no es premi l'opció 0 de sortida.
      *
      * Recomanacions:
@@ -156,24 +159,63 @@ public class Application {
      * "S'ha de seleccionar una opció correcta del menú."
      * - definiu una variable opcio de tipus enter.
      */
-    
+
+
+    public static void menuCampus() {
+        int opcio = 0;
+        int indexSel;
+        do {
+            System.out.println("\nMenú Campus. Selecciona una opció:");
+            System.out.println("\n0. Sortir");
+            System.out.println("\n1. Alta");
+            System.out.println("\n2. Modificar");
+            System.out.println("\n3. Llista Campus");
+            System.out.println("\n");
+
+            opcio = Integer.parseInt(DADES.nextLine());
+
+            switch (opcio) {
+                case 0:
+                    break;
+                case 1:
+                   universitatActual.addCampus();
+
+                    break;
+                case 2:
+                    indexSel = universitatActual.selectCampus(null);
+
+                    if (indexSel >= 0) {
+                        universitatActual.getCampus()[indexSel].updateCampus();
+                    } else {
+                        System.out.println("\nNo existeix aquest campus");
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < universitatActual.getpCampus(); i++){
+                        universitatActual.getCampus()[i].showCampus();
+                    }
+            }
+
+        } while (opcio != 0);
+    }
+
 
     /*
      * TODO
-     * 
+     *
      * Nom del mètode: menu Aules Estàndard
-     *     
+     *
      * Heu de desenvolupar el menuAulesEstandard amb les opcions que podeu veure.
      * Nota: penseu que quan arribem aquí, l'atribut universitatActual no és null
-     * 
+     *
      * Opció 0. Sortir -->       Surt del menú i retorna al menú principal
-     * Opció 1. Alta -->         Crea Aula estàndard del campus actual afegint-lo a un Campus. 
-     *                           Penseu que Universitat sap afegir Aules estàndard a una Campus seleccionat.       
-     * Opció 2. Modificar -->    Permet modificar AulaEstàndard del campus actual. Penseu que tots els 
-     *                           AulesEstandard d'un campus pertanyen a una Campus d'aquest campus i que 
+     * Opció 1. Alta -->         Crea Aula estàndard del campus actual afegint-lo a un Campus.
+     *                           Penseu que Universitat sap afegir Aules estàndard a una Campus seleccionat.
+     * Opció 2. Modificar -->    Permet modificar AulaEstàndard del campus actual. Penseu que tots els
+     *                           AulesEstandard d'un campus pertanyen a una Campus d'aquest campus i que
      *                           Universitat sap modificar AulaEstàndard que pertany a una dels seu Campus.
      * Opció 3. Llista AulesEstandard --> Imprimeix les dades de tots els AulesEstandard del campus actual.
-     *  
+     *
      * A més, heu de fer una estructura iterativa per tornar a mostrar el menú sempre que no es premi l'opció 0 de sortida
      *
      * Recomanacions:
@@ -181,56 +223,63 @@ public class Application {
      * - si no s'ha introduït cap opció de les de la llista, s'ha de mostrar el missatge "S'ha de seleccionar una opció correcta del menú."
      * - definiu una variable opcio de tipus enter
      */
-    
+public static void menuAulesEstandard(){
+
+}
 
     /*
      * TODO
-     * 
+     *
      * Nom del mètode: menuAulesInformatica
-     *    
+     *
      * Heu de desenvolupar el menuAulesInformatica amb les opcions que podeu veure.
      * Nota: penseu que quan arribem aquí, l'atribut universitatActual no és null
-     * 
+     *
      * Opció 0. Sortir -->       Surt del menú i retorna al menú principal
-     * Opció 1. Alta -->         Crea una AulaInformatica del campus actual afegint-la a un Campus. 
-     *                           Penseu que Universitat sap afegir una AulaInformatica a un Campus seleccionat.       
-     * Opció 2. Modificar -->    Permet modificar una AulaInformatica del campus actual. Penseu que totes les 
-     *                           AulesInformatica d'un campus pertanyen a una Campus d'aquest Campus i que 
+     * Opció 1. Alta -->         Crea una AulaInformatica del campus actual afegint-la a un Campus.
+     *                           Penseu que Universitat sap afegir una AulaInformatica a un Campus seleccionat.
+     * Opció 2. Modificar -->    Permet modificar una AulaInformatica del campus actual. Penseu que totes les
+     *                           AulesInformatica d'un campus pertanyen a una Campus d'aquest Campus i que
      *                           Universitat sap modificar una AulaInformatica que pertany a una de les seves Campus.
      * Opció 3. Llista AulesInformatica --> Imprimeix les dades de tots/es AulesInformatica del campus actual.
      * A més, heu de fer una estructura iterativa per tornar a mostrar el menú sempre que no es premi l'opció 0 de sortida
-     * 
+     *
      * Recomanacions:
      * - estructura de control switch-case per bifurcar les opcions
      * - si no s'ha introduït cap opció de les de la llista, s'ha de mostrar el missatge "S'ha de seleccionar una opció correcta del menú."
      * - definiu una variable opcio de tipus enter
      */
-    
+public static void menuAulesInformatica(){
+
+}
 
     /*
      * TODO
-     * 
+     *
      * Nom del mètode: menuLaboratoris
-     *     
+     *
      * Heu de desenvolupar el menuLaboratoris amb les opcions que podeu veure.
      * Nota: penseu que quan arribem aquí, l'atribut universitatActual no és null
-     * 
+     *
      * Opció 0. Sortir -->       Surt del menú i retorna al menú principal
-     * Opció 1. Alta -->         Crea una Apartament del campus actual afegint-la a una Campus. 
-     *                           Penseu que Universitat sap afegir un Apartament a una Campus seleccionat.       
-     * Opció 2. Modificar -->    Permet modificar un Apartament del campus actual. Penseu que totes els 
-     *                           Laboratoris d'un campus pertanyen a una Campus d'aquest Campus i que 
+     * Opció 1. Alta -->         Crea una Apartament del campus actual afegint-la a una Campus.
+     *                           Penseu que Universitat sap afegir un Apartament a una Campus seleccionat.
+     * Opció 2. Modificar -->    Permet modificar un Apartament del campus actual. Penseu que totes els
+     *                           Laboratoris d'un campus pertanyen a una Campus d'aquest Campus i que
      *                           Universitat sap modificar una Apartament que pertany a una dels seus Campus.
      * Opció 3. Llista Laboratoris --> Imprimeix les dades de tots els Laboratoris del campus actual.
-     * 
+     *
      * A més, heu de fer una estructura iterativa per tornar a mostrar el menú sempre que no es premi l'opció 0 de sortida
-     * 
+     *
      * Recomanacions:
      * - estructura de control switch-case per bifurcar les opcions
      * - si no s'ha introduït cap opció de les de la llista, s'ha de mostrar el missatge "S'ha de seleccionar una opció correcta del menú."
      * - definiu una variable opcio de tipus enter
      */
-    
+    public static void menuLaboratoris(){
+
+    }
+
 
     public static Integer selectUniversitat(Universitat Universitat) {
         String nom;
